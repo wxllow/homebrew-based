ee6dacfcbb05a91fb53d6f39db58f87ec4741437
willow <willow@wxllow.dev>
2022-10-02 11:45:21 -0400

    Update applemusicrp.rb

--- Casks/applemusicrp.rb
+++ Casks/applemusicrp.rb
@@ -1,6 +1,6 @@
 cask "applemusicrp" do
   version "3.2.1"
-  sha256 "fff8c2b090e93fe3762edcf98323c15c7ea9ac98942025ab5191be1cf41794c7"
+  sha256 "1f811d4b17d026f872f0e6b9a819654618ed112d1baaa1bb6427aec5"
 
   url "https://github.com/wxllow/applemusicrp/releases/download/#{version}/AppleMusicRP.dmg"
   name "AppleMusicRP"
